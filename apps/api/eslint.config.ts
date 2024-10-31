613f6bdfcecc1e4464de3df9e0d52ef4b80f5c09
Cuong Le <vinadiamon@gmail.com>
2024-10-31 23:18:43 +0700

    feat: init turbo repo & project setup

---
+++ apps/api/eslint.config.ts
@@ -0,0 +1,13 @@
+import { Linter } from 'eslint';
+
+const config: Linter.Config = {
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'error',
+  },
+};
+
+export default config;
---
+++ apps/web/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@mantine/core";
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">
+        Hello, Vite + React + TypeScript + Mantine + Tailwind!
+      </h1>
+      <Button className="mt-4">Mantine Button</Button>
+    </div>
+  );
+}
+
+export default App;
